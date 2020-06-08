7fe6a4faef5f56b3e710ce5ccfc9b1d8cf016307
andrewong9 <66503228+andrewong9@users.noreply.github.com>
2020-06-08 08:34:37 +0800

    1st Commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,77 +1,67 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        inventory
-      </h1>
-      <h2 class="subtitle">
-        My first-rate Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div class = "flex h-screen bg-gray-700">
+    <div class = "m-auto w-full px5 md:w-1/3">
+      <div class = "pl-1 text-white font-medium text-2xl, tracking wide text-center">
+        Inventory system{{email}}
+       </div> 
+    <form>
+      <div class = "my-2">
+      <inputElement
+        :labelName = "'Email'"
+        :labelId = "'email'"
+        :inputType = "'email'"
+        :model = "'email'"
+        @input = "email = $event"
+        :disabled = "false"
+      />
       </div>
+      <div class = "my-2">
+      <inputElement
+        :labelName = "'Password'"
+        :labelId = "'password'"
+        :inputType = "'password'"
+        :model = "'password'"
+        @input = "password = $event"
+        :disabled = "false"
+      />
+      </div>
+      <button type = "button" class = "
+           transition
+           duration-500
+           ease-in-out
+           bg-blue-600
+           rounded-xs
+           w-full
+           py-3
+           text-white
+           font-sans
+           font-bold
+           tracking-wide
+           hover:bg-blue-700
+           hover:text-gray-400
+           active:outline-none
+           focus:outline-none
+      ">SIGN IN</button>
+    </form>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import InputElement from '~/components/items/Input.vue';
+import Logo from  '~/components/items/Logo.vue';
 
-export default {
-  components: {
-    Logo
+export default{
+  components:{
+    Logo,
+    InputElement
+  },
+  data:function(){
+    return{
+      email:"",
+      password:""
+    }
   }
 }
+
 </script>
-
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
